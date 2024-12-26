3b1d15120617fb3d86efaa3fc499a68fc0edf330
Oguzhan Sali <salioguzhan.61@gmail.com>
2024-12-26 17:29:15 +0300

    added icons for side bar folders

---
+++ src/assets/icons/IconChevronDown.jsx
@@ -0,0 +1,21 @@
+// icon:chevron-down | Bootstrap https://icons.getbootstrap.com/ | Bootstrap
+import * as React from "react";
+
+function IconChevronDown(props) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z"
+      />
+    </svg>
+  );
+}
+
+export default IconChevronDown;
