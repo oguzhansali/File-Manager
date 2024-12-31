5f9a07c6577319caaf9c82d269f91adb0d7bd04d
Oguzhan Sali <salioguzhan.61@gmail.com>
2024-12-31 15:01:53 +0300

    checks selected checkboxes.

--- src/components/list-view/ListView.jsx
+++ src/components/list-view/ListView.jsx
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import "./ListView.Style.css"
 import {useViewContext} from "./../../context/view-context/view-context"
 
-
 const ListView = ({files,folders}) => {
-    const {select,dselect} = useViewContext()
+    const {setSelection,select,dselect,clear,itemIsSelected} = useViewContext()
     const handleSelectionChange = (e,item) =>{
-        const val = e.currentTarget.checked
+        const val = e.currentTarget.checked;
         if(val){
             select(item);
         }else{
@@ -14,17 +13,30 @@ const ListView = ({files,folders}) => {
         }
     };
 
+    const bulk = (e) => {
+        const val = e.currentTarget.checked;
+        if(val){
+            setSelection([...(folders || []),...(files || [])])  
+        }else{
+            clear();
+        }
+    }
+
     return(
     <div className="list-view">
         <div className="list-view-title list-view-item">
-            <input type="checkbox" />
+            <input type="checkbox" onChange={bulk}/>
             <span>Name</span>
         </div>
 
         {folders?.map((f)=>{
             return(
                 <div key={f.id} className="list-view-item">
-                    <input type="checkbox"  onChange={(e)=> handleSelectionChange(e,f)}/>
+                    <input 
+                    type="checkbox"  
+                    checked={Boolean(itemIsSelected(f))}
+                    onChange={(e)=> handleSelectionChange(e,f)}
+                    />
                     <img src="/folder.png"/>
                     <span>{f.name}</span>
                 </div>      
@@ -34,7 +46,10 @@ const ListView = ({files,folders}) => {
         {files?.map((f)=>{
             return(
                 <div key={f.id} className="list-view-item">
-                    <input type="checkbox" onChange={(e)=> handleSelectionChange(e,f)} />
+                    <input 
+                    type="checkbox" 
+                    checked={Boolean(itemIsSelected(f))}
+                    onChange={(e)=> handleSelectionChange(e,f)} />
                     <img src={f.url}/>
                     <span>{f.name}</span>
                 </div>      
