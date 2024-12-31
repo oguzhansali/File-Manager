5e0386a4557c45c6970eda3bcf0b444bf772ab04
Oguzhan Sali <salioguzhan.61@gmail.com>
2024-12-31 13:44:45 +0300

    added Toolbar

--- src/pages/FolderView.jsx
+++ src/pages/FolderView.jsx
@@ -6,12 +6,12 @@ import Main from "../layouts/Main";
 import TreeMenuItem from "../components/tree-menu/TreeMenuItem";
 import {useModal} from "../context/modal-context/modal-context"
 import CreateFolderModal from "../modals/CreateFolderModal/CreateFolderModal";
+import ListView from "../components/list-view/ListView";
+import Toolbar from "../components/toolbar/Toolbar";
 
 const FolderView = () => {
     const modal = useModal();
-
     const params = useParams();
-
     const folder = useFolderQuery(params.id);
 
     const name = !params.id || params.id==="null" ? "Kök Klasör" : folder.find.data?.name ;
@@ -30,12 +30,16 @@ const FolderView = () => {
 
     return (    
         <Main
+            toolbar={<Toolbar/>}
             folderName={name}
             sidebar={
                 <>
                     <button onClick={handleClickCreate} className="create-buttom">Create</button>
                     <TreeMenuItem name="Kök Klasör" id="null" defaultExpanded={true}/>
                 </>
+            }
+            content={
+            <ListView files={folder.files.data} folder={folder.list.data}/>
             }
         />
     )
