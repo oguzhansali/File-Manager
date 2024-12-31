422c60f3f60e686a87a3048009f5bde0391329bc
Oguzhan Sali <salioguzhan.61@gmail.com>
2024-12-31 13:52:41 +0300

    added ViewProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClientProvider,QueryClient } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import FolderView from './pages/FolderView.jsx'
 import  {ModalProvider}  from './context/modal-context/modal-context.jsx'
+import { ViewProvider } from './context/view-context/view-context.jsx'
 
 
 
@@ -13,14 +14,16 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <ModalProvider> 
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<FolderView />}/>
-          <Route path='folder/:id' element={<FolderView />}/>
-        </Routes>
-      </BrowserRouter>   
-    </ModalProvider>
+    <ViewProvider>
+      <ModalProvider> 
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<FolderView />}/>
+            <Route path='folder/:id' element={<FolderView />}/>
+          </Routes>
+        </BrowserRouter>   
+      </ModalProvider>
+    </ViewProvider>
   </QueryClientProvider>
   
 )
